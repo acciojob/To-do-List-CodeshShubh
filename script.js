583b8a95a12bb3ed17d4f5682a08f7ba9446ca43
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2025-01-13 00:39:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here
 
+const newTodoInput = document.getElementById("newTodoInput");
+const addTodoBtn = document.getElementById("addTodoBtn");
+const todoList = document.getElementById("todoList");
+
+
+addTodoBtn.addEventListener('click',()=>{
+	let todoText = newTodoInput.value.trim();
+	if(todoText ===""){
+		alert('please Enter Valid text First');
+		return;
+	}
+
+	let todoTag = document.createElement('li')
+	     todoTag.textContent = todoText;
+	   todoList.appendChild(todoTag)
+
+	newTodoInput.value ='';
+})
+
+
+
+
